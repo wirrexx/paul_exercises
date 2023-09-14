aa60ff929206dfc3eac4e16967ed11d4e3fdfdf1
Wisam Odish <wirrexx@gmail.com>
2023-09-14 14:58:17 +0200

    added comment

--- exercise_a.py
+++ exercise_a.py
@@ -1,12 +1,16 @@
 # variables for name, age and a message to my future self
+commentLine = "# variables for name, age and a message to my future self"
 name = "Wisam"
 age = 36
 message = "I will be a better programmer"
+height = 174.5
 
+print(commentLine)
 print(type(name))
 print(type(age))
 
 # By formatting my good, i can have a cleaner print statement. 
 print(f"Hi, my name is {name}, and I am {str(age)} years old, at the end of the school, {message}")
 
-"""It should be easier to exchange name, age and message for whomever is using this code"""
+"""It should be easier to exchange name, 
+age and message for whomever is using this code"""
