3f65799ec7eea7ebcaacae45be785a3a5ab71d7a
Wisam Odish <wirrexx@gmail.com>
2023-09-14 13:28:13 +0200

    testing Cast

---
+++ exercise_a.py
@@ -0,0 +1,14 @@
+import math
+
+myAge = 36
+myName = 'Wisam'
+myHeight = 173.5
+myGENDER = "M"
+
+myHeightNew = str(myHeight)
+
+print(type(myAge))
+print(type(myName))
+print(type(myHeightNew))
+print(isinstance(myHeight, float)) #isinstance kollar om variabel myHeight är en float
+print(isinstance(myHeightNew, int))
