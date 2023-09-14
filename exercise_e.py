32658c14486c82e7855995851ec8fdea924223c7
Wisam Odish <wirrexx@gmail.com>
2023-09-14 15:48:43 +0200

    Simplified

--- exercise_e.py
+++ exercise_e.py
@@ -4,15 +4,26 @@ complexNumber = 4-2j
 isMarried = True
 message = "'How are you?'"
 
-messageA = (f"Is {myNum} and instance of Int?: {(isinstance(myNum, int))}")
-messageB = (f"Is {myFloat} and instance of bool?: {(isinstance(myFloat, bool))}")
-messageC = (f"Is {complexNumber} and instance of complex?: {(isinstance(complexNumber, complex))}")
-messageD = (f"Is {isMarried} and instance of bool?: {(isinstance(isMarried, bool))}")
-messageE = (f"Is {message} and instance of float?: {(isinstance(message, float))}")
+# messageA = f"Is {myNum} an instance of Int?: {isinstance(myNum, int)}"
+# messageB = f"Is {myFloat} an instance of bool?: {isinstance(myFloat, bool)}"
+# messageC = f"Is {complexNumber} and instance of complex?: {isinstance(complexNumber, complex)}"
+# messageD = f"Is {isMarried} an instance of bool?: {isinstance(isMarried, bool)}"
+# messageE = f"Is {message} an instance of float?: {isinstance(message, float)}"
 
-print(messageA)
-print(messageB)
-print(messageC)
-print(messageD)
-print(messageD)
-print(messageE)
+messageA2 = "is ",str(myNum),"an instance of Int?: ", isinstance(myNum, int)
+messageA3 = "is ",str(myFloat),"an instance of bool?: ", isinstance(myFloat, bool)
+messageA4 = "Is", str(complexNumber), "an instance of complex?: ", isinstance(complexNumber, complex)
+messageA5 = "Is", isMarried, "an instance of bool?: ", isinstance(isMarried, bool)
+messageA6 = "Is", message, "an instance of float?: ", isinstance(isMarried, float)
+
+print(messageA2)
+print(messageA3)
+print(messageA4)
+print(messageA5)
+print(messageA6)
+# print(messageA)
+# print(messageB)
+# print(messageC)
+# print(messageD)
+# print(messageD)
+# print(messageE)
