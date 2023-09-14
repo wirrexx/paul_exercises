81b1cac3eb667c7ad2c3507a94dc77324c4a537f
Wisam Odish <wirrexx@gmail.com>
2023-09-14 15:10:44 +0200

    Excercise c

---
+++ excercise_c.py
@@ -0,0 +1,11 @@
+myNum = 123
+myFloat = 43.23
+complexNumber = 4-2j
+message = "How are you?"
+isMarried = True
+
+print(type(myNum))
+print(type(myFloat))
+print(type(complexNumber))
+print(type(message))
+print(type(isMarried))
