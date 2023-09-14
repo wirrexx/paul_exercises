acc394834f391e1b13babb4ecb76f5c1eb6d764f
Wisam Odish <wirrexx@gmail.com>
2023-09-14 15:22:11 +0200

    Excercise_d

---
+++ excercise_d.py
@@ -0,0 +1,16 @@
+placementWorld = 3
+cityName = "Stockholm"
+population = 1.870000
+countryName = "Sweden"
+isPopulated = True
+directNumber = +46-00j
+states = 26
+
+
+
+print(isinstance(placementWorld, int))
+print(isinstance(cityName, float))
+print(isinstance(isPopulated, bool))
+print(isinstance(directNumber, complex))
+print(isinstance(population, float))
+print(isinstance(states, str))
