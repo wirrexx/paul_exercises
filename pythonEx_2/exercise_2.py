5c5e77f7040f34fe4174db90babc0475a6b6aad7
Wisam Odish <wirrexx@gmail.com>
2023-09-15 14:31:31 +0200

    Added new Exercises

--- pythonEx_2/exercise_2.py
+++ pythonEx_2/exercise_2.py
@@ -4,7 +4,7 @@ print(max(2,5,6,7,12,41,444))
 print(min(2,5,6,7,12,41,444))
 print(abs(-240))
 print(pow(3,4))
-print(math.ceil(34/5))
-print(math.floor(34/5))
+print(math.ceil(34/5))  # svaret här är 6.8, ceiling rundar upp
+print(math.floor(34/5)) # svaret här är 6.8, floor rundar ner
 print(max(True, False))
 print(abs(4-3j))
