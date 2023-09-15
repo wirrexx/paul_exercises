b0bfa6ad294b94c12c92d6a0e51352250c30d7a7
Wisam Odish <wirrexx@gmail.com>
2023-09-15 10:54:32 +0200

    Updated folder

--- pythonEx_2/python_test.py
+++ pythonEx_2/python_test.py
@@ -2,20 +2,15 @@ x, y , z = "Red", "Green", "Blue" # Dont do this, for readability!
 
 
 #Do this, For readability! Easier to debug as Variables are more seperated.
-
 x = "Red"
 y = "Green"
-z = "blue"
+z = "Blue"
 
-cmlpxNum = 14-41j 
+myAge = None
 
-x = 10 
-y = 20 
-x, y = y,x
 
-exponantionalNum = 1e15
-highNum = 100_000_000_000_000_000_000
 
-print(x, y, sep=" , ")
-print(exponantionalNum)
-print(highNum)
+# Using bool() casting på olika datatyper
+
+print(bool())
+print(bool(""))  #Empty string = false  Written string = true
