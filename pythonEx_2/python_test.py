22d973cff629335dd68eeafecb6dde07bbbfb9ba
Wisam Odish <wirrexx@gmail.com>
2023-09-15 09:38:16 +0200

    Debug not working

--- pythonEx_2/python_test.py
+++ pythonEx_2/python_test.py
@@ -1,8 +1,15 @@
 x, y , z = "Red", "Green", "Blue" # Dont do this, for readability!
 
 
-#Do this, For readability!
+#Do this, For readability! Easier to debug as Variables are more seperated.
 
 x = "Red"
 y = "Green"
 z = "blue"
+
+
+x = 10 
+y = 20 
+x, y = y,x
+
+print(x, y, sep="")
