5afeee3fa0f996f4c4035b040f8e741a156c7ca9
Wisam Odish <wirrexx@gmail.com>
2023-10-10 09:39:57 +0200

    Updated with comments

---
+++ pythonEx_2/collection.py
@@ -0,0 +1,5 @@
+# Vad är en collection 
+# Låter dig visualisera set of item (collection)
+
+# string är också ett sett av character Hello world = h,e,l,l,o'',w,o,r,l,d
+# for loops för att accessera individuella "delar" av collectionen. 
