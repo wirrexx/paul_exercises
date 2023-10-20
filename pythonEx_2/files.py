47362e4d4d52d7893ac7b2dcc808394b4f7eeeac
Wisam Odish <wirrexx@gmail.com>
2023-10-20 11:55:20 +0200

    Updated files

--- pythonEx_2/files.py
+++ pythonEx_2/files.py
@@ -1,2 +1,14 @@
+from pprint import pprint
+f = open('turtles.csv', 'w')
+print (f.writable())
+f.write('Welcome to the world of files.')
+
+f.close()
+
+
 f = open('turtles.csv')
-print(f)
+
+lines = f.readlines()
+
+for x in lines:
+    print(x)
