fb4dba6c937790e8f33f3b5af01b5b4e1f7e57c9
Wisam Odish <wirrexx@gmail.com>
2023-10-25 11:04:29 +0200

    Websitecreation

--- pythonEx_2/testing.py
+++ pythonEx_2/testing.py
@@ -1,4 +1,4 @@
-my_string = "Hello Coder! You have done it! "
+""" my_string = "Hello Coder! You have done it! "
 
 iterator = iter(my_string)
 
@@ -8,4 +8,25 @@ while True:
     item = next(iterator, 'end')
     if item == 'end':
         break
-    print(item)
+    print(item) """
+
+
+import flet as ft 
+
+def main(page: ft.page):
+    # Använda koma tecken visar vad för parameter en funktion tar in
+    page.window_width=500
+    page.window_height=700
+    page.bgcolor="#45898"
+
+    
+    textField = ft.TextField(width=400, height=50)
+    addBtn = ft.ElevatedButton(text='Search')
+    entries_row=ft.Row(controls=[textField, addBtn]) # måste komma på slutet av koden
+
+    page.add(entries_row)
+
+
+
+
+ft.app(target=main)
